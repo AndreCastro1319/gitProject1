632309ff3cb6e995355580b352d5bbfeb83d141b
jimmycastro <ndrecastro@gmail.com>
2022-01-25 19:50:08 -0500

    first commit

---
+++ src/gitFolder/Main.java
@@ -0,0 +1,8 @@
+package gitFolder;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Yo no so y Tu marido ni tampoco tu hombre solo el cangri que cuando tu llama te responde...");
+    }
+}
